287473df08edb6a94eb3c7a02997f86d0d4bc26a
Mathushihan <ratnatheepanmathu@gmail.com>
2026-09-23 07:46:35 +0530

    Requestmapping learnt

--- src/main/java/Learn/io/HelloWorld/TodoController.java
+++ src/main/java/Learn/io/HelloWorld/TodoController.java
@@ -1,13 +1,22 @@
 package Learn.io.HelloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/Todo/Get")
 public class TodoController {
-    @GetMapping("/")
+    @GetMapping("/users")
     String toDo() {
         return  "TODO";
     }
 
+    @GetMapping("/allusers")
+    String toDoUsers() {
+        return  "TODO all users";
+    }
+
 }
+
+
